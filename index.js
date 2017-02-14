77a2962548773c09005c6411d641769178c834a5
Sandeep Reddy <msande1996@gmail.com>
2017-02-14 23:42:25 +0530

    Refactor code; bump V1

--- index.js
+++ index.js
@@ -1,65 +1,23 @@
-const electronLocalshortcut = require('electron-localshortcut');
 const {app, BrowserWindow} = require('electron');
 const electron = require('electron');
 
+var shortCutHandler = require('app_shortcuts');
+
 let win;
+let child;
 
 app.on("ready", createWindow);
 
-function likeClicked() {
-    win.webContents.executeJavaScript("document.getElementsByClassName('playbackSoundBadge__like')[0].click()");
-}
-
-function nextClicked() {
-    win.webContents.executeJavaScript("document.getElementsByClassName('playControls__next')[0].click()");
-}
-
-function prevClicked() {
-    win.webContents.executeJavaScript("document.getElementsByClassName('playControls__prev')[0].click()");
-}
-
-function pauseClicked() {
-    win.webContents.executeJavaScript("document.getElementsByClassName('playControls__play')[0].click()");
-}
-
-function repeatClicked(){
-    win.webContents.executeJavaScript("document.getElementsByClassName('repeatControl')[0].click()");
-}
-
-var map = {
-    'L': likeClicked,
-    'MediaNextTrack': nextClicked,
-    'N': nextClicked,
-    'MediaPreviousTrack': prevClicked,
-    'P': prevClicked,
-    'MediaPlayPause': pauseClicked,
-    'Space': pauseClicked,
-    'A':repeatClicked
-};
-
-function mapShortCuts() {
-    for (let key in map) {
-        electronLocalshortcut.register(win, key, () => {
-            map[key].call()
-        });
-    }
-}
-
 function createWindow() {
-
-    const {width, height} = electron.screen.getPrimaryDisplay().workAreaSize
-    win = new BrowserWindow({ width, height });
+    win = new BrowserWindow({ backgroundColor: '#FF6B00' });
     win.maximize();
     win.loadURL('https://www.soundcloud.com');
-    win.show();
-
-    mapShortCuts();
-
+    shortCutHandler.init(win);
 }
 
 
 app.on("window-all-closed", () => {
-    electronLocalshortcut.unregister(win, 'Ctrl+A');
-    electronLocalshortcut.unregisterAll(win);
+    shortCutHandler.dest();
     app.quit();
-})
+});
+
