6a3fcfda1e80964b6d6864d3f9a5cd97ce22fa5d
Sandeep Reddy <msande1996@gmail.com>
2017-05-05 01:45:42 +0530

    Add eslint && travis

--- main.js
+++ main.js
@@ -1,107 +1,115 @@
 const electron = require("electron");
-const {app, BrowserWindow} = require("electron");
+const { app, BrowserWindow } = require("electron");
 const path = require("path");
 const url = require("url");
-const {ipcMain,globalShortcut} = require("electron");
+const { ipcMain, globalShortcut } = require("electron");
 
 let win;
 
-app.on("ready", ()=>{
-    createWindow();
-    registerShorts();
-    electron.screen.on("display-metrics-changed",(event,display,metrics)=>{
-        console.log("Here");
-    });
+app.on("ready", () => {
+	createWindow();
+	registerShorts();
 });
 
-app.on("window-all-closed",()=>{
-    win = null;
-    app.quit();
+app.on("window-all-closed", () => {
+	win = null;
+	app.quit();
 });
 
 /**
  * Creates our main window
  */
-function createWindow(){
-    const {width, height} = electron.screen.getPrimaryDisplay().workAreaSize;
+function createWindow() {
+	const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize;
 
-    win = new BrowserWindow({
-        frame: false,
-        width : width,
-        height : height,
-        backgroundColor: '#333333',
-        title: 'Soundkeys',
-        autoHideMenuBar: true
-    });
+	win = new BrowserWindow({
+		frame: false,
+		width: width,
+		height: height,
+		backgroundColor: "#333333",
+		title: "Soundkeys",
+		autoHideMenuBar: true
+	});
 
-    win.maximize();
+	win.maximize();
 
-    var htmlUrl = url.format({
-        pathname : path.join(__dirname,"./src/index.html"),
-        protocol : "file:",
-        slashes : true
-    });
-    
-    win.loadURL(htmlUrl);
-    win.webContents.openDevTools("undocked");
+	var htmlUrl = url.format({
+		pathname: path.join(__dirname, "./src/index.html"),
+		protocol: "file:",
+		slashes: true
+	});
+
+	win.loadURL(htmlUrl);
+	win.webContents.on("new-window", function (event, url, frameName, disposition, windowOptions) {
+		windowOptions["node-integration"] = false;
+	});
+	win.webContents.openDevTools("undocked");
 }
 
 /**
  * Register the global shortcuts
  */
-function registerShorts(){
-	globalShortcut.register("MediaNextTrack",()=>{
-		win.webContents.send("shortCut","nextTrack");
+function registerShorts() {
+	globalShortcut.register("MediaNextTrack", () => {
+		win.webContents.send("shortCut", "nextTrack");
 	});
 
-	globalShortcut.register("MediaPreviousTrack",()=>{
-		win.webContents.send("shortCut","prevTrack");
+	globalShortcut.register("MediaPreviousTrack", () => {
+		win.webContents.send("shortCut", "prevTrack");
 	});
 
-	globalShortcut.register("MediaPlayPause",()=>{
-		win.webContents.send("shortCut","playTrack");
+	globalShortcut.register("MediaPlayPause", () => {
+		win.webContents.send("shortCut", "playTrack");
 	});
 
-	globalShortcut.register("MediaStop",()=>{
+	globalShortcut.register("MediaStop", () => {
 		//quit the app?
-        win.close();
+		win.close();
 	});
 
-	globalShortcut.register("CommandOrControl+3",()=>{
+	globalShortcut.register("CommandOrControl+3", () => {
 		//repeat toggle
-		win.webContents.send("shortCut","likeTrack");
+		win.webContents.send("shortCut", "likeTrack");
 	});
-	
-	globalShortcut.register("CommandOrControl+4",()=>{
+
+	globalShortcut.register("CommandOrControl+4", () => {
 		//like here
-		win.webContents.send("shortCut","repeatTrack");
+		win.webContents.send("shortCut", "repeatTrack");
 	});
 
 }
 
-function positionWin(){
-    var Positioner = require('electron-positioner')
-    var positioner = new Positioner(win);
-    pos = (process.platform==="win32") ? "bottomRight" : "topRight";
-    positioner.move(pos);
+function positionWin() {
+	var Positioner = require("electron-positioner");
+	var positioner = new Positioner(win);
+	var pos = (process.platform === "win32") ? "bottomRight" : "topRight";
+	positioner.move(pos);
 }
 
-ipcMain.on("mini_player",()=>{
-    win.setSize(200,200);
+// TODO: Remove if not required
+function clearCookies() {
+	electron.session.defaultSession.clearStorageData([],()=> {
+		console.log("Wazzap");
+	});
+}
+
+ipcMain.on("mini_player", () => {
+	win.setSize(200, 200);
+	positionWin();
 });
 
-ipcMain.on("min_win",()=>{
-    win.minimize();
+ipcMain.on("min_win", () => {
+	win.minimize();
 });
 
-ipcMain.on("max_win",()=>{
-    if(win.isMaximized()){
-        win.unmaximize();
-    }else{
-        win.maximize();
-    }
+ipcMain.on("max_win", () => {
+	if (win.isMaximized()) {
+		win.unmaximize();
+	} else {
+		win.maximize();
+	}
 });
 
-ipcMain.on("close_win",()=>{
-    win.close();
+ipcMain.on("close_win", () => {
+	win.close();
 });
