0158664c461048b779dde1d68d31e48cc67abea2
Sandeep Reddy <msande1996@gmail.com>
2017-05-07 18:53:29 +0530

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,10 +14,6 @@ module.exports = {
             "error",
             "tab"
         ],
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
         "quotes": [
             "error",
             "double"
